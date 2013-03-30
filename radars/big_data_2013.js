05325f0bad0fdad5b65228e73c8fc91f7b3b07e2
David Clarke <terrorhawks@gmail.com>
2013-03-30 12:53:21 +1100

    big data radar

---
+++ radars/big_data_2013.js
@@ -0,0 +1,20 @@
+var radar_data = [{"name":"Hadoop", "pc":{"r":120,"t":35},"movement":"c"},
+{"name":"Incremental data warehousing", "pc":{"r":250,"t":165},"movement":"c"},
+{"name":"Capability modelling", "pc":{"r":180,"t":125},"movement":"c"},
+{"name":"Service choreography", "pc":{"r":180,"t":105},"movement":"c"},
+{"name":"Visualisation and metrics", "pc":{"r":80,"t":150},"movement":"c"},
+{"name":"NoSQL", "pc":{"r":180,"t":65},"movement":"c"},
+{"name":"Neo4j", "pc":{"r":180,"t":50},"movement":"c"},
+{"name":"mongoDB", "pc":{"r":130,"t":55},"movement":"c"},
+];
+
+var radar_quadrants = [
+{"name":"Databases", "start":0, "end":4},
+{"name":"Data visualisation", "start":5, "end":6},
+{"name":"Machine learning", "start":47, "end":70},
+{"name":"Data mining/warehousing", "start":35, "end":46}
+];
+
+var radar_arcs = [{'r':100,'name':'Adopt'},{'r':200,'name':'Trial'},{'r':300,'name':'Assess'},{'r':400,'name':'Hold'}];
+
+var radar_title = "Big Data Technology Radar - 2013";
